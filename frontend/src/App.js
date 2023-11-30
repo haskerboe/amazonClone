9aa1ae0df0fa7117cdbd1153fce8f80e00d8b57f
Roma <haskerboe@gmail.com>
2023-11-30 20:47:27 +0500

    Add routing vid8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,22 @@
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product._id}>
-              <a href={`/product/${product._id}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product._id}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link href="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import data from '../data';
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product._id}>
+            <Link href={`/product/${product._id}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className="product-info">
+              <Link href={`/product/${product._id}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default HomeScreen;
