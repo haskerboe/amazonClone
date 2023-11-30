15618f8f50c33b8e52a99b051488c5d904269879
Roma <haskerboe@gmail.com>
2023-11-30 20:25:38 +0500

    List Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>list</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product._id}>
+              <a href={`/product/${product._id}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product._id}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
