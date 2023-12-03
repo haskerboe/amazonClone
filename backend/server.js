cee5da5fc77566208efb881a2e3c37f0af8dafdd
Roma <haskerboe@gmail.com>
2023-12-04 02:08:02 +0500

    generate sample products

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import data from "./data.js";
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -15,29 +17,10 @@ mongoose
   });
 
 const app = express();
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("server at 5000 port");
 });
