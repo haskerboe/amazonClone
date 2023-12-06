4951ee3a1e278e11d45c8635c7474302471b6301
Roma <haskerboe@gmail.com>
2023-12-06 21:24:16 +0500

    Ready to heroku

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.listen(port, () => {
   console.log("server at 5000 port");
 });
