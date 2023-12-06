c73cd1b69cb6af7f871a5c473490e016a63133c6
Roma <haskerboe@gmail.com>
2023-12-06 19:20:58 +0500

    Uref Profile Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import OrderHistoryScreen from "./screens/OrderHistoryScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -24,6 +25,7 @@ function App() {
     localStorage.removeItem("userInfo");
     localStorage.removeItem("shippingAddress");
     localStorage.removeItem("paymentMethod");
+    window.location.href = "/signin";
   };
   return (
     <BrowserRouter>
@@ -92,6 +94,7 @@ function App() {
                 path="/orderhistory"
                 element={<OrderHistoryScreen />}
               ></Route>
+              <Route path="/profile" element={<ProfileScreen />} />
             </Routes>
           </Container>
         </main>
